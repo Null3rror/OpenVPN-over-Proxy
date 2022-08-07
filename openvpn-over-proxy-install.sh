4369b96891697eba3dcb01d3f79674e90a7ab06c
Sepehr Razmyar <69770592+Null3rror@users.noreply.github.com>
2022-08-07 16:41:52 +0000

    Add "block-outside-dns"

--- openvpn-over-proxy-install.sh
+++ openvpn-over-proxy-install.sh
@@ -411,6 +411,7 @@ server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
 			echo 'push "dhcp-option DNS 94.140.15.15"' >> /etc/openvpn/server/server.conf
 		;;
 	esac
+	echo 'push "block-outside-dns"' >> /etc/openvpn/server/server.conf
 	echo "keepalive 10 120
 cipher AES-256-CBC
 user nobody
